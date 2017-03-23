e0cc8610b83be4ca8c1bb73ec09b403daf4f12b9
Chad Durkin and Derek Villars <chad+derek@>
2017-03-23 16:36:32 -0700

    add functioning site, needs css still

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,10 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    // Ember.RSVP.has(
-    return {
+    return Ember.RSVP.hash({
       games: this.store.findAll('game'),
       categories: this.store.findAll('category')
-    };
+    });
   }
 });
